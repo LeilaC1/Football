811af7514ab2223bf32ef08c993cacb8e7e5ee10
LeilaC1 <leilaclesca13@gmail.com>
2023-11-02 22:07:40 -0400

    started teams

---
+++ teams.js
@@ -0,0 +1,17 @@
+"use strict";
+
+
+window.onload = function() {
+let teams = [
+    {code:"DAL", name:"Dallas Cowboys", plays:"Arlington, TX"},
+    {code:"DEN", name:"Denver Broncos", plays:"Denver, CO"},
+    {code:"HOU", name:"Houston Texans", plays:"Houston, TX"},
+    {code:"KAN", name:"Kansas City Chiefs",  plays:"Kansas City, MO"},
+   ];
+
+
+   const teamsList = document.getElementById("team");
+ let length = teams.length;
+ for (let i = 0; i < length; i++) 
+
+} 
